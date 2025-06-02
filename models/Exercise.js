aaf141da449e5226875119f28b67459a7ea20309
stu <stumac1000@yahoo.com>
2025-06-02 15:57:20 -0700

    Install packages

---
+++ models/Exercise.js
@@ -0,0 +1,33 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+
+const ExerciseSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+    },
+    sets: {
+      type: Number,
+    },
+    reps: {
+      type: Number,
+    },
+    measurement: {
+      type: Number,
+      required: [true, "Please provide a measurement"],
+    },
+    measurementUnit: {
+      type: String,
+      required: [true, "Please provide a unit of measurement"],
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Exercise", ExerciseSchema);
