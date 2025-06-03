1062a56ec8634d9fab188a63c76fa3eaf0dc9da4
stu <stumac1000@yahoo.com>
2025-06-02 17:46:20 -0700

    Create partials files

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+require("express-async-errors");
 require("./lib/env-vars-checker");
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.set("view engine", "ejs");
+app.use(require("body-parser").urlencoded({ extended: true }));
+
+// secret word handling
+app.get("/secretWord", (req, res) => {
+  if (!req.session.secretWord) {
+    req.session.secretWord = "syzygy";
+  }
+  res.render("secretWord", { secretWord: req.session.secretWord });
+});
+app.post("/secretWord", (req, res) => {
+  req.session.secretWord = req.body.secretWord;
+  res.redirect("/secretWord");
+});
+
+app.use((req, res) => {
+  res.status(404).send(`That page (${req.url}) was not found.`);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+  console.log(err);
+});
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
