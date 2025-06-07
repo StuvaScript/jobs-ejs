d488331c8a50a1f7b794eeeaa294ae3bb7e0287e
stu <stumac1000@yahoo.com>
2025-06-07 13:14:47 -0700

    Add authentication checking middleware

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require("./lib/env-vars-checker");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const url = process.env.MONGO_URI;
+const secretWordRouter = require("./routes/secretWord");
+const auth = require("./middleware/auth");
 
 app.use(require("body-parser").urlencoded({ extended: true }));
 app.use(require("connect-flash")());
@@ -49,25 +51,7 @@ app.use("/sessions", require("./routes/sessionRoutes"));
 app.set("view engine", "ejs");
 
 // secret word handling
-app.get("/secretWord", (req, res) => {
-  if (!req.session.secretWord) {
-    req.session.secretWord = "syzygy";
-  }
-  res.locals.info = req.flash("info");
-  res.locals.errors = req.flash("error");
-  res.render("secretWord", { secretWord: req.session.secretWord });
-});
-
-app.post("/secretWord", (req, res) => {
-  if (req.body.secretWord.toUpperCase()[0] == "P") {
-    req.flash("error", "That word won't work!");
-    req.flash("error", "You can't use words that start with p.");
-  } else {
-    req.session.secretWord = req.body.secretWord;
-    req.flash("info", "The secret word was changed.");
-  }
-  res.redirect("/secretWord");
-});
+app.use("/secretWord", auth, secretWordRouter);
 
 app.use((req, res) => {
   res.status(404).send(`That page (${req.url}) was not found.`);
